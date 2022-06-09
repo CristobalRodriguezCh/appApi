b763329a29129b4d787bcca1a156dc067e94a782
Cristóbal Rodríguez <cantoniorodriguez2307@gmail.com>
2022-06-09 10:24:37 -0500

    se agrego fk de autores a libros

--- app/Models/libro.php
+++ app/Models/libro.php
@@ -9,5 +9,9 @@ class libro extends Model
 {
     use HasFactory;
     protected $table ='libro';//nombre de la tabla de la base de datos
-    protected $fillable = ['titulo','autor','numero_paginas','precio','estado'];
+    protected $fillable = ['titulo','autor','numero_paginas','precio','estado','autor_id'];
+
+    public function autores(){
+        return $this->hasMany('App\Models\autor','autor_id');
+    }
 }
